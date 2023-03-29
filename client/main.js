f5a3384e5ca4e855aefeabcf64c0706be5457dc1
matheusccastro <camargodecastromatheus@gmail.com>
2023-03-29 10:24:55 -0300

    first commit

---
+++ client/main.js
@@ -0,0 +1,43 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  this.showContent = new ReactiveVar(false);
+  this.shouldShowContent = new ReactiveVar(false);
+
+  Tracker.autorun(async (computation) => {
+    const show = this.shouldShowContent.get()
+    // Simulate some async methods
+    await Tracker.withComputation(computation, () => new Promise(r => setTimeout(r, 5000)));
+
+    this.showContent.set(show);
+  })
+});
+
+Template.hello.helpers({
+  showContent() {
+    return Template.instance().showContent.get();
+  }
+});
+
+Template.hello.events({
+  'click #show'(event, instance) {
+    // If we want to manipulate the hidden content, this would work if we were not using async tracker
+    instance.shouldShowContent.set(true);
+    Tracker.flush();
+
+    // But it fails here, because we don't have the guarantees anymore that the UI is updated.
+    const selector = $("#hidden-content-main");
+    if (!selector.length) {
+      alert("Selector not found!")
+    } else {
+      selector.text("It worked!!")
+    }
+  },
+  'click #hide'(event, instance) {
+    instance.shouldShowContent.set(false);
+    instance.showContent.set(false);
+  },
+});
